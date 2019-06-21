6bd6b0e4c27db77c04fc33651925bcc7a4e04e98
tnals634 <tnals634@naver.com>
2019-06-21 16:21:46 +0900

    변수 넘기기

--- Thread_1/Program.cs
+++ Thread_1/Program.cs
@@ -7,22 +7,33 @@ using System.Threading;
 
 namespace Thread_1
 {
-    class Test
-    {
-        public void Print()
-        {
-            Console.WriteLine("Test!");
-        }
-    }
-
     class Program
     {
 
+        static void Func()
+        {
+            Console.WriteLine("스레드에서 호출");
+        }
+
+        static void ParameterizedFunc2(object obj)
+        {
+            for(int i=0;i<(int)obj;i++)
+            {
+                Console.WriteLine("Parameterized 스레드에서 호출 : {0}",i);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Test test = new Test();
-            Thread th = new Thread(new ThreadStart(test.Print));
-            th.Start();
+            ////Thread th = new Thread(new ThreadStart(Func));
+
+            //ThreadStart thStart = new ThreadStart(Func);
+            //Thread th = new Thread(thStart);
+            //th.Start();
+
+            int i = 5;
+            Thread th2 = new Thread(new ParameterizedThreadStart(ParameterizedFunc2));
+            th2.Start(i);
         }
     }
 }
