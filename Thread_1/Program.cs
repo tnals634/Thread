c98d9f05b33ba125534b5ab9d1162f09c7749490
tnals634 <tnals634@naver.com>
2019-06-21 16:51:51 +0900

    스레드의 속성_1(IsBackground)

--- Thread_1/Program.cs
+++ Thread_1/Program.cs
@@ -9,29 +9,22 @@ namespace Thread_1
 {
     class Program
     {
-        static void Func1()
+        static void Func()
         {
-            for(int i = 0;i<10;i++)
+            int i = 0;
+            while(true)
             {
-                Console.WriteLine("스레드 호출 {0}",i);
+                Console.WriteLine("{0}",i++);
+                Thread.Sleep(300);
             }
         }
 
-        static void Func2()
-        {
-            for(int i=10;i<20;i++)
-            {
-                Console.WriteLine("스레드 호출 {0}",i);
-            }
-        }
+        
         static void Main(string[] args)
         {
-            Thread th1 = new Thread(new ThreadStart(Func1));
-            Thread th2 = new Thread(new ThreadStart(Func2));
-
-            th1.Start();
-            th2.Start();
-
+            Thread th = new Thread(new ThreadStart(Func));
+            th.IsBackground = true; //th는 백그라운드로 실행해라
+            th.Start();
             Console.WriteLine("메인 종료");
         }
     }
