828a1f93f46ad42fb9a6e6ddf3d4efcf13acf6e6
tnals634 <tnals634@naver.com>
2019-06-21 16:39:48 +0900

    두개 이상의 스레드로 객체 메소드 호출

--- Thread_1/Program.cs
+++ Thread_1/Program.cs
@@ -7,29 +7,27 @@ using System.Threading;
 
 namespace Thread_1
 {
-
+    class Test
+    {
+        public void Print()
+        {
+            Console.WriteLine("스레드 호출");
+        }
+    }
     
     class Program
     {
-        static void Func1()
-        {
-            Console.WriteLine("스레드1 호출");
-        }
-
-        static void Func2()
-        {
-            Console.WriteLine("스레드2 호출");
-        }
-
+        
         static void Main(string[] args)
         {
-            Thread th1 = new Thread(new ThreadStart(Func1));
-            Thread th2 = new Thread(new ThreadStart(Func2));
+            Test test1 = new Test();
+            Test test2 = new Test();
+
+            Thread th1 = new Thread(new ThreadStart(test1.Print));
+            Thread th2 = new Thread(new ThreadStart(test2.Print));
 
             th1.Start();
             th2.Start();
-
-            Console.WriteLine("메인 종료");
         }
     }
 }
