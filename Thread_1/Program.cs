93e41c94b68333888554bd874b0eab25fcbbc757
tnals634 <tnals634@naver.com>
2019-06-21 16:43:18 +0900

    두개 이상의 스레드 함수가 반복

--- Thread_1/Program.cs
+++ Thread_1/Program.cs
@@ -7,27 +7,32 @@ using System.Threading;
 
 namespace Thread_1
 {
-    class Test
-    {
-        public void Print()
-        {
-            Console.WriteLine("스레드 호출");
-        }
-    }
-    
     class Program
     {
-        
+        static void Func1()
+        {
+            for(int i = 0;i<10;i++)
+            {
+                Console.WriteLine("스레드 호출 {0}",i);
+            }
+        }
+
+        static void Func2()
+        {
+            for(int i=10;i<20;i++)
+            {
+                Console.WriteLine("스레드 호출 {0}",i);
+            }
+        }
         static void Main(string[] args)
         {
-            Test test1 = new Test();
-            Test test2 = new Test();
-
-            Thread th1 = new Thread(new ThreadStart(test1.Print));
-            Thread th2 = new Thread(new ThreadStart(test2.Print));
+            Thread th1 = new Thread(new ThreadStart(Func1));
+            Thread th2 = new Thread(new ThreadStart(Func2));
 
             th1.Start();
             th2.Start();
+
+            Console.WriteLine("메인 종료");
         }
     }
 }
