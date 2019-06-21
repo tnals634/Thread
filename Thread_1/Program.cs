ba64295cd0420205044500451f0e74d214e79c3a
tnals634 <tnals634@naver.com>
2019-06-21 16:57:36 +0900

    Thread 속성 중 CurrentThread

--- Thread_1/Program.cs
+++ Thread_1/Program.cs
@@ -9,22 +9,18 @@ namespace Thread_1
 {
     class Program
     {
-        static void Func()
+        static void Func1()
         {
-            int i = 0;
-            while(true)
-            {
-                Console.WriteLine("{0}",i++);
-                Thread.Sleep(300);
-            }
+            Console.WriteLine("2번 스레드 : {0}",Thread.CurrentThread.GetHashCode());
         }
 
         
         static void Main(string[] args)
         {
-            Thread th = new Thread(new ThreadStart(Func));
-            th.IsBackground = true; //th는 백그라운드로 실행해라
+            Thread th = new Thread(new ThreadStart(Func1));
+            
             th.Start();
+            Console.WriteLine("메인 스레드 {0}", Thread.CurrentThread.GetHashCode());
             Console.WriteLine("메인 종료");
         }
     }
