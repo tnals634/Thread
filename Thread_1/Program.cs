fb2c8c9c8f54f4b91513cd15b2a769eb5653ed2e
tnals634 <tnals634@naver.com>
2019-06-21 16:20:24 +0900

    객체 메소드 넘기기

---
+++ Thread_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread_1
+{
+    class Test
+    {
+        public void Print()
+        {
+            Console.WriteLine("Test!");
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            Thread th = new Thread(new ThreadStart(test.Print));
+            th.Start();
+        }
+    }
+}
